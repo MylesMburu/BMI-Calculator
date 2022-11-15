e41210affbd349e716fc9fa0755ebf7a356f0547
MylesMburu <ngicumyles@gmail.com>
2022-11-15 12:36:57 +0300

    BMI calc

---
+++ Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtBMI = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtRemark = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(181, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(398, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "BMI(Body Mass Index) Calculator"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(52, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(289, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Please enter your weight in Kilograms(kgs)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(52, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(254, 18)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Please enter your height in Metres(m)"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.OliveDrab
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(247, 202)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(217, 35)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'txtWeight
+        '
+        Me.txtWeight.AccessibleName = ""
+        Me.txtWeight.Location = New System.Drawing.Point(407, 99)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(100, 22)
+        Me.txtWeight.TabIndex = 4
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(407, 140)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(100, 22)
+        Me.txtHeight.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(55, 271)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(88, 18)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Your BMI is:"
+        '
+        'txtBMI
+        '
+        Me.txtBMI.Location = New System.Drawing.Point(225, 270)
+        Me.txtBMI.Name = "txtBMI"
+        Me.txtBMI.Size = New System.Drawing.Size(214, 22)
+        Me.txtBMI.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(55, 334)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(152, 18)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "This is considered as:"
+        '
+        'txtRemark
+        '
+        Me.txtRemark.Location = New System.Drawing.Point(225, 334)
+        Me.txtRemark.Name = "txtRemark"
+        Me.txtRemark.Size = New System.Drawing.Size(214, 22)
+        Me.txtRemark.TabIndex = 9
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.OliveDrab
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(247, 388)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(217, 28)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtRemark)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtBMI)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.txtWeight)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtWeight As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtBMI As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtRemark As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim weight As Double
+        weight = Val(txtWeight.Text)
+        Dim height As Double
+        height = Val(txtHeight.Text)
+        Dim bmi As Double
+
+        bmi = weight / (height * height)
+
+        If bmi < 18.5 Then
+            txtBMI.Text = bmi
+            txtRemark.Text = "UNDERWEIGHT"
+        ElseIf bmi >= 18.5 And bmi <= 24.9 Then
+            txtBMI.Text = bmi
+            txtRemark.Text = "NORMAL"
+        ElseIf bmi >= 25 And bmi <= 29.9 Then
+            txtBMI.Text = bmi
+            txtRemark.Text = "OVERWEIGHT"
+        ElseIf bmi >= 30 And bmi <= 39.9 Then
+            txtBMI.Text = bmi
+            txtRemark.Text = "OBESE"
+        ElseIf bmi > 40 Then
+            txtBMI.Text = bmi
+            txtRemark.Text = "MORBID OBESE"
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txtHeight.Text = " "
+        txtWeight.Text = " "
+        txtBMI.Text = " "
+        txtRemark.Text = " "
+    End Sub
+End Class
